edddc18f130873ad049d071ae76fc21e8690a66a
Ervin Bara <ervinbara17@gmail.com>
2023-10-12 17:34:39 +0200

    no cm

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,13 +19,22 @@ class ProductController extends AbstractController
         $this->entityManager = $entityManager;
     }
     #[Route('/nos-produits', name: 'app_products')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        } else {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
 
         return $this->render('product/index.html.twig', [
             'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 
